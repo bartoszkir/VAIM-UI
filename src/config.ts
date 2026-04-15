d036e25860b9b27bc9b5b6496677d78175b2119e
Kirker <Bartosz.Kirker@dnv.com>
2026-04-15 08:43:03 +0200

    Config

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 export default {
   applicationName: import.meta.env.VITE_APPLICATION_NAME || "AI Marketplace",
-  apiBaseUrl: import.meta.env.VITE_API_BASE_URL || "http://localhost:4000/api",
+  apiBaseUrl: import.meta.env.VITE_API_BASE_URL || "https://vaim-app-hzdhefe2cwhybzge.northeurope-01.azurewebsites.net/api",
   signInUrl:
-    import.meta.env.VITE_SIGN_IN_URL || "http://localhost:4000/api/auth/signin",
+    import.meta.env.VITE_SIGN_IN_URL || "https://vaim-app-hzdhefe2cwhybzge.northeurope-01.azurewebsites.net/api/auth/signin",
   signOutUrl:
     import.meta.env.VITE_SIGN_OUT_URL ||
-    "http://localhost:4000/api/auth/signout",
+    "https://vaim-app-hzdhefe2cwhybzge.northeurope-01.azurewebsites.net/api/auth/signout",
 };
